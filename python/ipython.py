39fdffad20c43ceb8a683e8da3bf6906daa68b4c
fmind <dev@fmind.me>
2018-07-31 13:27:29 +0200

    improved python config

---
+++ python/ipython.py
@@ -0,0 +1,11 @@
+# Configuration file for ipython.
+
+c.TerminalInteractiveShell.colors = "Linux"
+c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.confirm_exit = False
+
+c.InteractiveShellApp.extensions = []
+
+c.InteractiveShellApp.exec_files = []
+
+c.InteractiveShellApp.exec_lines = ["import re", "from importlib import reload"]
--- python/ipython_config.py
+++
@@ -1,7 +0,0 @@
-# Configuration file for ipython.
-
-c = get_config()
-
-c.TerminalInteractiveShell.colors = 'Linux'
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.confirm_exit = False
