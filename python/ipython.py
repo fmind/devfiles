fe3d95dbfbea0d954d4469402bdba042ea6b8c52
Médéric Hurier <dev@fmind.me>
2018-08-17 13:36:42 +0200

    improved python

--- python/ipython.py
+++ python/ipython.py
@@ -3,9 +3,3 @@
 c.TerminalInteractiveShell.colors = "Linux"
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.confirm_exit = False
-
-c.InteractiveShellApp.extensions = []
-
-c.InteractiveShellApp.exec_files = []
-
-c.InteractiveShellApp.exec_lines = []
