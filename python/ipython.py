7d4df40ac4318f4de9ca842636d5007b3e5d563d
Médéric Hurier <dev@fmind.me>
2018-10-03 16:25:13 +0200

    improved python language

--- python/ipython.py
+++ python/ipython.py
@@ -1,5 +1,20 @@
 # Configuration file for ipython.
 
+c.Completer.use_jedi = True
+
+c.TerminalIPythonApp.force_interact = True
+
 c.TerminalInteractiveShell.colors = "Linux"
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
+
+c.InteractiveShellApp.extensions = [
+    'autoreload',
+    'storemagic',
+]
+
+c.InteractiveShellApp.exec_lines = [
+    'import re',
+    '%autoreload 2',
+]
