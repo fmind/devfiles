fb9872cad656efedd5e8f7d6207f623044603f3b
Médéric Hurier <dev@fmind.me>
2018-10-05 08:39:08 +0200

    clean up python

--- python/ipython.py
+++ python/ipython.py
@@ -7,14 +7,9 @@ c.TerminalIPythonApp.force_interact = True
 c.TerminalInteractiveShell.colors = "Linux"
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.confirm_exit = False
+
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 
-c.InteractiveShellApp.extensions = [
-    'autoreload',
-    'storemagic',
-]
+c.InteractiveShellApp.extensions = ["autoreload", "storemagic"]
 
-c.InteractiveShellApp.exec_lines = [
-    'import re',
-    '%autoreload 2',
-]
+c.InteractiveShellApp.exec_lines = ["import re", "%autoreload 2"]
