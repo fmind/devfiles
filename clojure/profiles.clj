09f9e5e9e1c0adf53e75f05998ab5743cf83bc4c
Médéric Hurier <dev@fmind.me>
2018-10-31 18:04:21 +0100

    added clojure

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,10 +1,1 @@
-{
-  :repl {:global-vars {*print-length* 1000}}
-
-  :user {:dependencies [[spyscope "0.1.6"]]
-         :plugins [[lein-ancient "0.6.15"]]
-         ; :global-vars {*warn-on-reflection* true}
-         :injections [(require 'spyscope.core)
-                      (require '[clojure.repl :refer :all])
-                      (require '[clojure.pprint :refer :all])]}
-}
+{:repl {:global-vars {*print-length* 1000}}}
