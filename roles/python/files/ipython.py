bf9f0a14cf8cb57f26aa8cec3686970c174a0627
Médéric Hurier <dev@fmind.me>
2018-11-09 22:32:58 +0100

    modified ipython

--- roles/python/files/ipython.py
+++ roles/python/files/ipython.py
@@ -1,15 +1,7 @@
 # Configuration file for ipython.
 
-c.Completer.use_jedi = True
-
-c.TerminalIPythonApp.force_interact = True
-
-c.TerminalInteractiveShell.colors = "Linux"
-c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.editing_mode = "emacs"
-
 c.InteractiveShellApp.extensions = ["autoreload"]
-
 c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
 
+c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
