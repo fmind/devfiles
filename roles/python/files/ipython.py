505d81727858ebe8d42f4eadd305395d532829a2
Médéric Hurier <dev@fmind.me>
2018-11-20 15:44:00 +0100

    improved python layer

--- roles/python/files/ipython.py
+++ roles/python/files/ipython.py
@@ -1,7 +1,13 @@
 # Configuration file for ipython.
 
-c.InteractiveShellApp.extensions = ["autoreload"]
-c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+c.InteractiveShellApp.extensions = [
+    "autoreload"
+]
+
+c.InteractiveShellApp.exec_lines = [
+    "%autoreload 2",
+    "from importlib import reload",
+]
 
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
