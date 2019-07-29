a070e1b285c9b06859af52b6f2f4440b039f71a1
Médéric Hurier (fmind) <fmind@fmind.me>
2019-07-29 10:59:16 +0200

    disable jupytext

--- roles/jupyter/files/jupyter_notebook_config.py
+++ roles/jupyter/files/jupyter_notebook_config.py
@@ -2,4 +2,4 @@
 
 # c.ContentsManager.default_jupytext_formats = "ipynb,py:light"
 # c.ContentsManager.preferred_jupytext_formats_save = "py:light"
-c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+# c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
