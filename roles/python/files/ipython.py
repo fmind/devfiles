118713d4696e1b465ab1d5c859d774c55b40778c
Médéric Hurier (fmind) <fmind@fmind.me>
2019-08-09 13:56:16 +0200

    added deepreload

--- roles/python/files/ipython.py
+++ roles/python/files/ipython.py
@@ -3,12 +3,12 @@
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 
-InteractiveShellApp.extensions = [
+c.InteractiveShellApp.extensions = [
     'autoreload',
     'storemagic',
 ]
 
 c.InteractiveShellApp.exec_lines = [
     '%autoreload 2',
-    'from IPython.lib import deepreload as reload'
+    'from IPython.lib.deepreload import reload',
 ]
