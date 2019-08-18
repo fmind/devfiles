5f80fdcf15a643ec7597d6414add8d3c163246d2
Médéric Hurier (fmind) <fmind@fmind.me>
2019-08-18 15:36:25 +0200

    format profiles.clj

--- roles/clojure/files/profiles.clj
+++ roles/clojure/files/profiles.clj
@@ -1,13 +1,7 @@
-{
-  :repl {
-    :global-vars {*print-length* 1000
-                  *warn-on-reflection* true}
-    :injections [
-      (use 'clojure.datafy)
-      (use 'clojure.java.javadoc)
-      (use 'clojure.java.shell)
-      (use 'clojure.pprint)
-      (use 'clojure.repl)
-    ]
-  }
-}
+{:repl {:injections [(use 'clojure.datafy)
+                     (use 'clojure.java.javadoc)
+                     (use 'clojure.java.shell)
+                     (use 'clojure.pprint)
+                     (use 'clojure.repl)]
+        :global-vars {*print-length* 1000
+                      *warn-on-reflection* true}}}
