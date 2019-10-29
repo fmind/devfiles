84b099a7d4be28fba753674e624f7702179fe5cf
Médéric Hurier (fmind) <fmind@fmind.me>
2019-10-29 21:32:21 +0100

    added vscode

--- roles/python/files/ipython.py
+++ roles/python/files/ipython.py
@@ -4,12 +4,5 @@ c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 
 c.InteractiveShellApp.exec_lines = [
-    'import re',
-    'import pydoc',
-    'from inspect import *',
-    'from functools import *',
-    'from itertools import *',
-    'from collections import *',
-    'from pathlib import Path',
     'from IPython.lib.deepreload import reload',
 ]
