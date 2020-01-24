117f97480bf9b9ff1e80aa6b79a7ce0ce6dc680a
Médéric Hurier <fmind@fmind.me>
2020-01-24 22:08:20 +0100

    refactored

---
+++ roles/python/files/ipython.py
@@ -0,0 +1,10 @@
+# Configuration file for ipython.
+
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
+
+c.InteractiveShellApp.exec_lines = [
+    '%load_ext autoreload',
+    'from IPython.lib.deepreload import reload',
+]
