5394800c9ec62e4b068b6ab4e4a3d99b53ff6d2c
Médéric Hurier <fmind@fmind.me>
2020-04-23 06:59:49 +0200

    updated to ubuntu 20.04

--- roles/clojure/files/profiles.clj
+++ roles/clojure/files/profiles.clj
@@ -1,3 +1,3 @@
-{:repl {:injections [(use 'clojure.pprint)
-                     (use 'clojure.repl)]
+{:repl {:injections [(use 'clojure.repl)
+                     (use 'clojure.pprint)]
         :global-vars {*print-length* 1000}}}
